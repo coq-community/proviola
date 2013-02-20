d2379c5706a3f037297a330b787b2e35d250df70
Carst Tankink <3909851+Carst-Tankink@users.noreply.github.com>
2013-02-20 15:36:48 +0100

    [b+] Use the Python3.2 branch/fixes of subprocess to create a toplevel. Fixes hanging when calling Coq from a multithreaded situations (such as in the test suite)

--- camera/toplevel.py
+++ camera/toplevel.py
@@ -1,6 +1,6 @@
 """ Toplevel emulation from python. """
 # The solutions here are *Nix-specific.
-import subprocess
+import subprocess32 as subprocess
 import fcntl, os
 
 class Toplevel(subprocess.Popen):
@@ -8,7 +8,7 @@ class Toplevel(subprocess.Popen):
     kargs['stdin'] = subprocess.PIPE
     kargs['stderr'] = subprocess.PIPE
     kargs['stdout'] = subprocess.PIPE
+    kargs['close_fds'] = True
     super(Toplevel, self).__init__(*args, **kargs)
-
     fcntl.fcntl(self.stdout, fcntl.F_SETFL, os.O_NONBLOCK) 
     fcntl.fcntl(self.stderr, fcntl.F_SETFL, os.O_NONBLOCK)
